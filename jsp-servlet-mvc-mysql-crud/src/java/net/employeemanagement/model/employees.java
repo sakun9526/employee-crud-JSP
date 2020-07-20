2538b8819ec55909cf7f1a9f6b33b5ba700ac0e5
sakun9526 <sakunrathnayaka@yahoo.com>
2020-07-21 00:06:01 +0530

    employees java bean class updated

--- jsp-servlet-mvc-mysql-crud/src/java/net/employeemanagement/model/employees.java
+++ jsp-servlet-mvc-mysql-crud/src/java/net/employeemanagement/model/employees.java
@@ -1,14 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package net.employeemanagement.model;
 
-/**
- *
- * @author Sakun
- */
 public class employees {
+    
+    private int id;
+    private String name;
+    private String email;
+    private int nic;
+    
+    public employees (int id , String name, String email, int nic ){
+        super();
+        this.id = id;
+        this.name = name;
+        this.email= email;
+        this.nic = nic;
+    }
+    
+    public employees (String name, String email, int nic ){
+        super();
+        this.name = name;
+        this.email= email;
+        this.nic = nic;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id; 
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name; 
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email; 
+    }
+    
+    public int getNic(){
+        return nic;
+    }
+    
+    public void setNic(int nic){
+        this.nic = nic; 
+    }
     
 }
